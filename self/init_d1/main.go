8c2a035db8b64ccf7d430da14158597a3a4395bd
bushibigniu <534987457@qq.com>
2019-05-23 23:54:39 +0800

    Create main.go

---
+++ self/init_d1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	_ "io"
+	"io/ioutil"
+	"net/http"
+)
+
+//v2 加字符版
+func main()  {
+	url := "http://www.zhenai.com/zhenghun"
+	resq, err := http.Get(url)
+
+	if err != nil{
+		panic(err)
+	}
+
+	defer resq.Body.Close()
+
+	if resq.StatusCode != http.StatusOK {
+		fmt.Println("response code :", resq.StatusCode)
+		return
+	}
+
+
+ 
+  	data, err := ioutil.ReadAll(utf8Reader)
+
+
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Printf("%s \n", data)
+
+}
