b3b20d7a6b6063019422a5ebceb2350eae27d05f
name <email>
2019-05-25 22:35:01 +0800

    update

---
+++ self/crawer3/engine/engine.go
@@ -0,0 +1,39 @@
+package engine
+
+import (
+	"go-study/self/crawer3/fetcher"
+	"log"
+)
+
+func Run(seed ...Request) {
+
+	var requests []Request
+	for _, r := range seed {
+		requests = append(requests, r)
+	}
+
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+
+		log.Printf("fetcher url : %s", r.Url)
+
+		body, err := fetcher.Fetch(r.Url)
+		if err != nil {
+			log.Printf("fetcher error fetcher url %s: %v", r.Url, err)
+			continue
+		}
+
+		parseResult := r.ParserFunc(body)
+
+		//requests = append(requests, parseResult.Requests[0])
+		//requests = append(requests, parseResult.Requests[1])
+		//这是个集合， 用... 代替所有
+		requests = append(requests, parseResult.Requests...)
+
+		for _, item := range parseResult.Items {
+			log.Printf("get item  %v", item)
+		}
+	}
+
+}
---
+++ self/crawer3/parser/profile.go
@@ -0,0 +1,84 @@
+package parser
+
+import (
+	"fmt"
+	"go-study/self/crawer3/engine"
+	"go-study/self/crawer3/model"
+	"regexp"
+)
+
+//<div data-v-bff6f798="" class="m-btn purple">未婚</div>
+const regexpInfo  = `<div (.*?)class="m-btn purple"(.*?)>([^<]+)</div>`
+const regexpInfo2  = `<div (.*?)class="m-btn pink"(.*?)>([^<]+)</div>`
+
+const age  = `<td><span class="label">年龄：</span>([\d]+)岁</td>`
+
+//废弃 因为正则不一样
+func ParseUserProfile2(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(age)
+
+	fmt.Print("begin")
+
+	age := re.FindSubmatch(contents)
+
+	fmt.Println(age)
+	fmt.Printf("age %s",age)
+
+
+	return engine.ParseResult{}
+}
+
+
+func ParseUserProfile(content []byte, name string) engine.ParseResult {
+	re := regexp.MustCompile(regexpInfo)
+	//re2 := regexp.MustCompile(regexpInfo2)
+
+	// info 是类似这样的集合
+//[<div class="m-btn purple" data-v-bff6f798>高中及以下</div>   data-v-bff6f798 高中及以下]]
+	info := re.FindAllSubmatch(content, -1)
+
+	var profile model.Profile
+	profile.Name = name
+	for k, v := range info{
+		//把 if 条件写在这，和 封装成 dealValue 调用结果是一样的
+		//用指针方式， 和 不用指针 用return 回来 结果是一样的
+		//profile = dealValue(k, string(v[3]), profile)
+		dealValue(k, string(v[3]), &profile)
+	}
+
+	result := engine.ParseResult{
+		Items:[]interface{}{profile},
+	}
+
+	return result
+}
+
+//可以优化
+//func dealValue(k int, value string, profile model.Profile) (model.Profile){
+func dealValue(k int, value string, profile *model.Profile){
+
+	if k ==0 {
+		profile.Marriage = value
+	}
+
+	if k ==1 {
+		profile.Age = value
+	}
+	if k ==2 {
+		profile.Xinzuo = value
+	}
+	if k ==3 {
+		profile.Height = value
+	}
+	if k ==4 {
+		profile.Weight = value
+	}
+	if k ==7 {
+		profile.Occupation = value
+	}
+	if k ==6 {
+		profile.Income = value
+	}
+
+	//return profile
+}
