26693f613ee8a64b14442f9fc5d856aea2a2d4cf
name <email>
2019-05-29 22:31:45 +0800

    update

---
+++ self/15bingfa/crawer3/engine/concurrent.go
@@ -0,0 +1,66 @@
+package engine
+
+import (
+	"log"
+)
+
+type ConcurrentEngine struct {
+	Scheduler Scheduler
+	WorkerCount int
+
+}
+
+//add scheduler func
+type Scheduler interface {
+	Submit(Request)
+	ConfigureMasterWorkChan(chan Request)
+}
+
+//scheduler 实现第一步：所有worker 公用一个输入
+func (e *ConcurrentEngine) Run(seed ...Request) {
+
+	in := make(chan Request)
+	out := make(chan ParseResult)
+	e.Scheduler.ConfigureMasterWorkChan(in)
+
+	for i:=0;i< e.WorkerCount ; i++ {
+		//这边的worker 做：把scheduler 的 in  处理完， 输出 out 给 engine
+		createWorker(in, out)
+	}
+
+	for _, r := range seed {
+		e.Scheduler.Submit(r)
+	}
+
+	for  {
+		//这边的out 和上面的对应， 这时候 engine 收到的 out(也就是 requests, items)
+		//要给 scheduler 处理
+		result := <- out
+		for _,item := range result.Items{
+			log.Printf("got item : %v", item)
+		}
+
+		//这边掉起 scheduler
+		for _,request := range result.Requests{
+			//这边是engine 送 request 给 scheduler
+			//是直接调用，而不是通过channel
+			e.Scheduler.Submit(request)
+		}
+	}
+
+}
+
+func createWorker(in chan Request, out chan ParseResult)  {
+
+	//不断从 in 收集request, 不断 out 处理
+	go func() {
+		for  {
+			request := <- in
+			result , err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- result
+		}
+	}()
+}
